3cd9ba0e511638df1782d4d3074a1c877f52f307
jeffzyliu <jeffzyliu@gmail.com>
2020-08-15 21:24:47 -0400

    yay react native!

---
+++ navigation/search_tab.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import VideoList from '../components/video_list';
+import VideoDetail from '../components/video_detail';
+
+const Stack = createStackNavigator();
+
+// nest stack navigator to handle two internal views
+// "name" prop is the name of the route
+const SearchTab = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Search" component={VideoList} />
+      <Stack.Screen name="Detail" component={VideoDetail} />
+    </Stack.Navigator>
+  );
+};
+
+export default SearchTab;
---
+++ youtube-api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyDpK5DAfNayl5PIRqUS6tfh2PSksbQmgDc';
+
+const youtubeSearch = (term) => {
+  const params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video',
+    maxResults: 10,
+  };
+
+  // try refactoring to async later?
+  return new Promise((resolve, reject) => {
+    axios.get(API_URL, { params })
+      .then((response) => {
+        resolve(response.data.items);
+      })
+      .catch((error) => {
+        console.log(`youtube api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+export default youtubeSearch;
